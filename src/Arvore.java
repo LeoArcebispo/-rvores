2e091babd9b7debd9373e1ab9a2296153ca67ed9
LeoArcebispo <leonardo123btas@gmail.com>
2025-05-22 19:37:39 -0300

    preordem e posordem

--- src/Arvore.java
+++ src/Arvore.java
@@ -15,4 +15,20 @@ public class Arvore {
             percorrerEmOrdem(Node.FilhoDireito);
         }
     }
+
+    public void percorrerPreOrdem(No Node){
+        if(Node != null){
+            System.out.println(Node.valor + " ");
+            percorrerPreOrdem(Node.FilhoEsquerdo);
+            percorrerPreOrdem(Node.FilhoDireito);
+        }
+    }
+
+    public void percorrerPosOrdem(No Node){
+        if(Node != null){
+            percorrerPosOrdem(Node.FilhoEsquerdo);
+            percorrerPosOrdem(Node.FilhoDireito);
+            System.out.println(Node.valor + " ");
+        }
+    }
 }
