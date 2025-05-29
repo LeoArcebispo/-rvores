6107c7a16059e8f97b3a7379136f76853d6dc8b8
LeoArcebispo <leonardo123btas@gmail.com>
2025-05-29 20:19:53 -0300

    Balanceamento

--- src/No.java
+++ src/No.java
@@ -2,9 +2,11 @@ public class No {
     String valor;
     No FilhoEsquerdo;
     No FilhoDireito;
+    int Balanceamento;
 
-    public No(String valor){
+    public No(String valor, int balanceamento){
         this.valor = valor;
         FilhoEsquerdo = FilhoDireito = null;
+        Balanceamento = balanceamento = 0;
     }
 }
